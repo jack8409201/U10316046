e9b0cec056970fe4a40dd32894a5d1ec3e545e88
jack8409201 <jack8409201@gmail.com>
2015-03-27 23:49:13 +0800

    Modify Account class
    修改8.7的Account class 為題目規定: 新增name,ArrayList

---
+++ U10316046_HW3_11_8/src/Account.java
@@ -0,0 +1,75 @@
+/**
+ * Name: Jou Dan-Haur
+ * ID: U10316046
+ * Ex: 11.8
+ */
+public class Account{
+	//Declare data fields
+	private int id = 0;
+	private double balance = 0;
+	private double annualInterestRate = 0;
+	private java.util.Date dateCreated;
+	private String name;
+	private java.util.ArrayList transactions = new java.util.ArrayList();
+
+	//A no-arg constructor constructs to default value
+	public Account(){
+	}
+	//A constructor specifies id and initialize balance
+	public Account(int id, double balance, String name){
+		dateCreated = new java.util.Date();
+		this.id = id;
+		this.balance = balance;
+		this.name = name;
+	}
+	//Set the id
+	public void setId(int id){
+		this.id = id;
+	}
+	//Set the balance
+	public void setBalance(double balance){
+		this.balance = balance;
+	}
+	//Set the annual InterestRate
+	public void setAnnualInterestRate(double annualInterestRate){
+		this.annualInterestRate = annualInterestRate;
+	}
+	//Get the id
+	public int getId(){
+		return id;
+	}
+	//Get the balance
+	public double getBalance(){
+		return balance;
+	}
+	//Get the annual InterestRate
+	public double getAnnualInterestRate(){
+		return annualInterestRate;
+	}
+	public String getName() {
+		return name;
+	 }
+	//Get the monthly InterestRate
+	public double getMonthlyInterestRate(){
+		return annualInterestRate/12;
+	}
+	//Get the monthly Interest
+	public double getMonthlyInterest(){
+		return (balance*getMonthlyInterestRate())/100;
+	}
+	//Withdraw the money
+	public void withdraw(double amount){
+		balance -= amount;
+	}
+	//Deposit the money
+	public void deposit(double amount){
+		balance += amount;
+	}
+	//Get the dateCreated
+	public java.util.Date getDateCreated() {
+		return dateCreated;
+	}
+	public java.util.ArrayList getTransactions() {
+		return transactions;
+	}
+}
