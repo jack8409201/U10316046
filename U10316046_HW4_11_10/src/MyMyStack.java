206e6d7d453503ecde2a59a13a66263d25e857d8
jack8409201 <jack8409201@gmail.com>
2015-04-11 14:54:54 +0800

    Modify MyMyStack class
    新增註解

--- U10316046_HW4_11_10/src/MyMyStack.java
+++ U10316046_HW4_11_10/src/MyMyStack.java
@@ -5,33 +5,38 @@
  */
 import java.util.ArrayList;
 public class MyMyStack extends MyStack{
+	//Create an ArrayList store five strings
+	private ArrayList<String> list = new ArrayList<String>();
 		
-		private ArrayList<Object> list = new ArrayList<Object>();
-		
-		@Override
-		public boolean isEmpty(){
-			return list.isEmpty();
-		}
-		
-		@Override
-		public int getSize(){
-			return list.size();
-		}
-		
-		@Override
-		public Object peek(){
-			return list.get(getSize()-1);
-		}
-		
-		@Override
-		public Object pop(){
-			Object o = list.get(getSize()-1);
-			list.remove(getSize()-1);
-			return o;
-		}
-		
-		public void push(String o){
-			list.add(o);
-		}
+	//Override MyStack isEmpty method	
+	@Override
+	public boolean isEmpty(){
+		return list.isEmpty();
+	}
+	
+	//Override MyStack getSize method
+	@Override
+	public int getSize(){
+		return list.size();
+	}
+	
+	//Override MyStack peek method
+	@Override
+	public Object peek(){
+		return list.get(getSize()-1);
+	}
+	
+	//Override MyStack pop method
+	@Override
+	public Object pop(){
+		Object o = list.get(getSize()-1);
+		list.remove(getSize()-1);
+		return o;
+	}
+	
+	//Store string in the ArrayList
+	public void push(String o){
+		list.add(o);
+	}
 
 }
