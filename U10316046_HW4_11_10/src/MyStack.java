83951bc82c879a635ea626c048e59a3f249776ea
jack8409201 <jack8409201@gmail.com>
2015-04-17 17:49:58 +0800

    MyStack class
    修改課本程式MyStack繼承ArrayList

--- U10316046_HW4_11_10/src/MyStack.java
+++ U10316046_HW4_11_10/src/MyStack.java
@@ -4,41 +4,34 @@
  * Ex: 11.10
  */
 import java.util.ArrayList;
-public class MyStack {
+public class MyStack extends ArrayList<Object>{
 	
-	//Create an ArrayList store objects
-	private ArrayList<Object> list = new ArrayList<Object>();
+	private ArrayList<String> list = new ArrayList<String>();
 	
-	//Bollean method if stack is empty that return true
 	public boolean isEmpty() {
-		return list.isEmpty();
+		return super.isEmpty();
 	}
 	
-	//Method return amount of element in the stack
 	public int getSize() {
-		return list.size();
+		return super.size();
 	}
 
-	//Method return top object of the stack but not delete
 	public Object peek() {
-		return list.get(getSize() - 1);
+		return super.get(getSize() - 1);
 	}
 
-	//Method delete top object in the stack and return it
 	public Object pop() {
-		Object o = list.get(getSize() - 1);
-		list.remove(getSize() - 1);
+		Object o = super.get(getSize() - 1);
+		super.remove(getSize() - 1);
 		return o;
 	}
 	
-	//Method store object on the top 
 	public void push(Object o) {
-		list.add(o);
+		super.add(o);
 	}
 	
-	//Override the toString in the Object class
 	@Override
 	public String toString() {
-		return "stack: " + list.toString();
+		return "stack: " + super.toString();
 	}
 }
